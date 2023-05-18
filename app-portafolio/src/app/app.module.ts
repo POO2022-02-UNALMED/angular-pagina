c6fedb3b9adbbc14ca99188b01b5b6322cf959b5
Paula Misas <pmisas@unal.edu.co>
2023-05-18 18:23:29 -0500

    angular layouts

--- app-portafolio/src/app/app.module.ts
+++ app-portafolio/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
