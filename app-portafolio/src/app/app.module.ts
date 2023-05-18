0eec4b79d15cadafbdfa7f2d4dbd0fb2795b13b0
Paula Misas <pmisas@unal.edu.co>
2023-05-18 18:03:30 -0500

    estructura angular

---
+++ app-portafolio/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    // CORE
+
+    CoreModule,
+    SharedModule,
+    AppRoutingModule
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app-portafolio/src/app/shared/components/index.ts
@@ -0,0 +1,7 @@
+// Import components
+
+export const components: any[] = [
+
+];
+
+// Export all components
