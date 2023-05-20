0c16f79b7132c64b1764dc7b19d89f837851889f
Paula Misas <pmisas@unal.edu.co>
2023-05-20 01:17:53 -0500

    fake backend

--- app-portafolio/src/app/modules/user/user-detail/user-detail.component.ts
+++ app-portafolio/src/app/modules/user/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { USERS_DATA } from '@data/constants/user.constant';
+import { ICardUser } from '@shared/components/cards/card-user/card-user.metadata';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,11 +9,17 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent {
+public users: ICardUser[] = USERS_DATA;
+public currentUser?: ICardUser;
 public id: number;
 
 constructor(
   private route: ActivatedRoute
 ) {
   this.id = this.route.snapshot.params['id']; //toma el id de la ruta ya que es localhost:400/id
+  this.currentUser = this.users.find(user => user.id === +this.id);  //encontrar un user que cumpla que la id buscada sea a la propiedad id
+  console.log(this.currentUser)
 }
+
+
 }
--- app-portafolio/src/app/modules/user/user-list/user-list.component.ts
+++ app-portafolio/src/app/modules/user/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { USERS_DATA } from '@data/constants/user.constant';
 import { ICardUser } from '@shared/components/cards/card-user/card-user.metadata';
 
 @Component({
@@ -7,72 +8,6 @@ import { ICardUser } from '@shared/components/cards/card-user/card-user.metadata
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
-  public users: ICardUser[] = [
-    {
-      id: 1,
-      name: 'Paula Misas',
-      age: 22,
-      description: 'Estudiante de ingenieria de sistemas. aprendiendo angular',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 2,
-      name: 'Juan',
-      age: 25,
-      description: 'Estudiante de ingenieria de sistemas. full stack',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 3,
-      name: 'Luza',
-      age: 60,
-      description: 'Master. arquitecta',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 4,
-      name: 'Juan',
-      age: 25,
-      description: 'Estudiante de ingenieria de sistemas. full stack',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 5,
-      name: 'Juan',
-      age: 25,
-      description: 'Estudiante de ingenieria de sistemas. full stack',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 6,
-      name: 'Juan',
-      age: 25,
-      description: 'Estudiante de ingenieria de sistemas. full stack',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 7,
-      name: 'Juan',
-      age: 25,
-      description: 'Estudiante de ingenieria de sistemas. full stack',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    {
-      id: 8,
-      name: 'Juan',
-      age: 25,
-      description: 'Estudiante de ingenieria de sistemas. full stack',
-      avatar: 'https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg'
-  
-    },
-    
-  ]
+  public users: ICardUser[] = USERS_DATA;
 
 }
