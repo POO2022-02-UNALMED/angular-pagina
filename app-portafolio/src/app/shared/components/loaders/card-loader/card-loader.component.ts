b1821a811f6083e27337abf4b917702f0d75f290
Paula Misas <pmisas@unal.edu.co>
2023-05-21 16:08:44 -0500

    loading cards :100:

--- app-portafolio/src/app/shared/components/loaders/card-loader/card-loader.component.ts
+++ app-portafolio/src/app/shared/components/loaders/card-loader/card-loader.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-loader',
   templateUrl: './card-loader.component.html',
   styleUrls: ['./card-loader.component.css']
 })
-export class CardLoaderComponent {
+export class CardLoaderComponent implements OnInit {
 
   // Input styles
 
@@ -15,16 +15,36 @@ export class CardLoaderComponent {
 
   // Final properties
 
-  public totalBars = [];
+  public totalBars: {width: string}[] = [];
   public finalStyleImage = {};
-  public finalStyleBar = {};
+  public finalHeightBar = '0';
 
-
-
-  // calcular barras con bucle for
-  for (let: i = 0; i < this.bars; i++) {
-    this.totalBars.push(i)
+  constructor(){
   }
 
+  ngOnInit() {
 
+    // calcular barras con bucle for
+  
+    for (let i = 0; i < this.bars; i++) {
+      const widthRandom = Math.floor(Math.random() * (100 - 60)) + 60;
+      this.totalBars.push({width: `${widthRandom}%`});
+    }
+
+    // calcular ancho de la imagen de carga
+
+    this.finalStyleImage = {
+      width: `${this.imageSize}px`,
+      height: `${this.imageSize}px`
+    }
+
+    // calcular style barras
+
+    this.finalHeightBar = `${this.barHeight}px`
+    
+    
+
+  }
+  
 }
+
