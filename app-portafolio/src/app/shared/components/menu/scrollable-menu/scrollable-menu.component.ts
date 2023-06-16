b92937afc9dac5f19fc4413e09184b93d375f752
Paula Misas <pmisas@unal.edu.co>
2023-06-16 04:01:44 -0500

    colaboradores modal task

--- app-portafolio/src/app/shared/components/menu/scrollable-menu/scrollable-menu.component.ts
+++ app-portafolio/src/app/shared/components/menu/scrollable-menu/scrollable-menu.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ICoworker } from '@shared/components/cards/card-tasks/card-tasks.metadata';
 
 @Component({
   selector: 'app-scrollable-menu',
   templateUrl: './scrollable-menu.component.html',
   styleUrls: ['./scrollable-menu.component.css']
 })
+
 export class ScrollableMenuComponent {
+  @Input() workers:Array<ICoworker>
+
+  
+
+
 
 }
